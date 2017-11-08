194979855a52354df8880fc60e07e01d61472b5f
Rodrigo Oliveira <rodrigo.oliveira@trigg.com.br>
2017-11-08 20:06:24 -0200

    corrigindo problema com rota de login default

--- src/app/cadastro/cadastro.module.ts
+++ src/app/cadastro/cadastro.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import {AngularFireDatabase} from 'angularfire2/database';
@@ -9,16 +8,11 @@ import {PessoaComponent} from './pessoa/pessoa.component';
 import { FormComponent } from './form/form.component';
 import { ListaComponent } from './lista/lista.component';
 
-const ROUTES: Routes = [
-  { path: '', component: PessoaComponent}
-];
-
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule,
-    RouterModule.forChild(ROUTES)
+    ReactiveFormsModule
   ],
   providers: [AngularFireDatabase],
   declarations: [PessoaComponent, FormComponent, ListaComponent],
--- src/app/route/route.module.ts
+++ src/app/route/route.module.ts
@@ -3,12 +3,13 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from '../login/login.component';
+import { PessoaComponent } from '../cadastro/pessoa/pessoa.component';
 
 const APP_ROUTES: Routes = [
-  { path: 'cadastro', loadChildren: '../cadastro/cadastro.module#CadastroModule'},
+  { path: 'cadastro', component: PessoaComponent},
   { path: 'login', component: LoginComponent },
   { path: '', component: LoginComponent },
-]
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(APP_ROUTES)],
