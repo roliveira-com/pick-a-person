cdab729c66319381fe35eeeb422035b92a08c5b1
Rodrigo Oliveira <rodrigo.oliveira@trigg.com.br>
2017-11-08 19:39:58 -0200

    importando angular fire no modulo de login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
+import {AngularFireAuth} from 'angularfire2/auth';
+
 import { LoginComponent } from './login.component';
 
 @NgModule({
